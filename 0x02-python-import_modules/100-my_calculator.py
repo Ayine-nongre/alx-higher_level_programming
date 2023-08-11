bafa5ebaad250114774d92d2aba596d212e895d1
Ayine-nongre <eugeneatinbire@gmail.com>
2023-08-11 23:35:04 +0000

    Adding adv task 1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int((sys.argv)[1])
+    b = int((sys.argv)[3])
+
+    if (sys.argv)[2] == "+":
+        ans = calc.add(a, b)
+        print("{:d} + {:d} = {:d}".format(a, b, ans))
+    elif (sys.argv)[2] == "-":
+        ans = calc.sub(a, b)
+        print("{:d} - {:d} = {:d}".format(a, b, ans))
+    elif (sys.argv)[2] == "*":
+        ans = calc.mul(a, b)
+        print("{:d} * {:d} = {:d}".format(a, b, ans))
+    elif (sys.argv)[2] == "/":
+        ans = calc.add(a, b)
+        print("{:d} / {:d} = {:d}".format(a, b, ans))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
