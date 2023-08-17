e72dda16277bfe338ad5e540984c6124a1294660
Ayine-nongre <eugeneatinbire@gmail.com>
2023-08-17 21:48:15 +0000

    Editing task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return
-    unique = set(my_list)
+    if my_list:
+        unique = set(my_list)
 
-    summ = 0
-    for j in unique:
-        summ = summ + j
-    return summ
+        summ = 0
+        for j in unique:
+            summ = summ + j
+        return summ
