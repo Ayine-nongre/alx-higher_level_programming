c612b72631e631a36222319479d8730230a121b7
Ayine-nongre <eugeneatinbire@gmail.com>
2023-09-05 15:32:33 +0000

    Today's tasks solution

---
+++ 0x08-python-more_classes/0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(obj=[]):
+    obj += ["BestSchool"]
+    return ", ".join(obj)
---
+++ 0x08-python-more_classes/0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+dynamically creates a class that allows only first_name variable to be created
+"""
+
+class LockedClass:
+    __slots__ = ['first_name']
+    def __init__(self):
+        """
+        passes
+        """
+
+        pass
