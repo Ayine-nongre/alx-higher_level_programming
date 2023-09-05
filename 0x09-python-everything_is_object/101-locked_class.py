1ea2553778aea03cdeb24c93ba028c5728bb8eb0
Ayine-nongre <eugeneatinbire@gmail.com>
2023-09-05 15:57:07 +0000

    Editing 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,15 @@
 dynamically creates a class that allows only first_name variable to be created
 """
 
+
 class LockedClass:
+    """
+    dynamically creates a class that allows only first_name
+    variable to be created
+    """
+
     __slots__ = ['first_name']
+
     def __init__(self):
         """
         passes
