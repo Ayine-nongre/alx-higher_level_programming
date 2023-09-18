cbe2ad570372ef080197fc2b314feb4916e528fe
Ayine-nongre <eugeneatinbire@gmail.com>
2023-09-18 19:38:51 +0000

    Correcting rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,7 +104,7 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """Method to update field"""
 
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:
             list_atr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_atr[i], args[i])
