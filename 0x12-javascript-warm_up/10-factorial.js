a48348616a154593f214324aff3a3d93365316a8
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-13 20:26:42 +0000

    Adding remaining task

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (x) {
+  if (x === 0) {
+    return 1;
+  }
+
+  if (isNaN(x)) {
+    return 1;
+  }
+
+  return (x * factorial(x - 1));
+}
+
+const fact = factorial(parseInt(process.argv[2]));
+console.log(fact);
