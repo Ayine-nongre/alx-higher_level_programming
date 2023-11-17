570356b2dfb2952176242c5d4631238512eb6d3d
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-17 15:22:28 +0000

    Adding task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Query to filter states in ORM
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=N+")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
