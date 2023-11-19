2b9b7c2234d5e2772811507216a6d271e4ea5a5e
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-19 13:29:37 +0000

    Adding task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""
+Query that is protected against SQL injection to get data from DB
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities WHERE state = (%s)", (sys.argv[4],))
+    cities = cur.fetchall()
+
+    for city in cities:
+        print(city)
