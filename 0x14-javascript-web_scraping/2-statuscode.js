d5ffdf88a46d12fd901bb09f44977dac5f46e757
Ayine-nongre <eugeneatinbire@gmail.com>
2024-01-03 18:22:28 +0000

    D

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-
-request(process.argv[2], function (err, response, body) {
-  if (err) console.log(err);
-  if (response) console.log('code: ', response.statusCode);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
