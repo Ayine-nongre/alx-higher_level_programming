7a7d80bdc2c0357b3fd4b518a1d815145db31f74
Ayine-nongre <eugeneatinbire@gmail.com>
2024-01-03 10:40:02 +0000

    A

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require("fs")
+const fs = require('fs');
 
-fs.readFile(process.argv[2], ({encoding: 'utf-8'}), function(err, data){
-    if (err) console.log(err);
-    console.log(data)
+fs.readFile(process.argv[2], ({ encoding: 'utf-8' }), function (err, data) {
+  if (err) console.log(err);
+  if (data) console.log(data);
 });
